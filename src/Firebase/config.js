617f5cac0d15169443c2aa188560aa84780f461d
Toan Le <thanhtoanhcm1602@gmail.com>
2024-07-29 09:18:06 +0700

    - Fix AuthUI instance is deleted!  react-firebaseui.

--- src/Firebase/config.js
+++ src/Firebase/config.js
@@ -1,14 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-// import firebase from 'firebase/compat/app';
 
 // Configure Firebase.
 const firebaseConfig = {
   apiKey: 'AIzaSyCVciy0imK-HCeA9fJDTtRHhSeIpRxuWS4',
   authDomain: 'authreactjsfirebaseui.firebaseapp.com',
-  // ...
+  projectId: "authreactjsfirebaseui",
+  storageBucket: "authreactjsfirebaseui.appspot.com",
+  messagingSenderId: "731464871587",
+  appId: "1:731464871587:web:9e76c3e7548f7b9c249916",
+  measurementId: "G-LH9XQ6CYW1"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
--- src/features/Auth/pages/SignIn/index.jsx
+++ src/features/Auth/pages/SignIn/index.jsx
@@ -1,9 +1,7 @@
 import React from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../../../../Firebase/config";
-import { StyledFirebaseAuth } from "react-firebaseui";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
-// const googleProvider = new GoogleAuthProvider();
 
 // Configure FirebaseUI.
 const uiConfig = {
@@ -14,14 +12,7 @@ const uiConfig = {
   signInSuccessUrl: "/admin",
 
   // We will display Google and Facebook as auth providers.
-  // signInOptions: [auth.GoogleAuthProvider.PROVIDER_ID],
   signInOptions: [GoogleAuthProvider.PROVIDER_ID],
-
-  // Want to go to onAuthStateChanged,so not use callback.
-  // callbacks: {
-  //   // Avoid redirects after sign-in.
-  //   signInSuccessWithAuthResult: () => false,
-  // },
 };
 
 function SignIn(props) {
@@ -29,13 +20,7 @@ function SignIn(props) {
     <div>
       <h1>My App</h1>
       <p>Please sign-in:</p>
-      {/* <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} /> */}
-      {/* <button
-        style={{ width: "100%", marginBottom: 5 }}
-        // onClick={() => handleLogin(googleProvider)}
-      >
-        Login by Google
-      </button> */}
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
     </div>
   );
 }
